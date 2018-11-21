1e33b256ed34117485889117ff44244341b8b9bc
idg23 <40529961+idg23@users.noreply.github.com>
2018-11-20 21:11:39 -0500

    Add files via upload

--- build_site.R
+++ build_site.R
@@ -2,10 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/Ishan/personal_cite/personal_site")
+setwd("/Users/Ishan/Adelia/personal_website")
 
 #render your sweet site. 
 rmarkdown::render_site()
-
-
-
